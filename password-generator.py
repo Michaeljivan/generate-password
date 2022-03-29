7959057be844c358f1886609e1897224bf242fc4
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2022-03-29 15:46:19 -0700

    Create password-generator.py

---
+++ password-generator.py
@@ -0,0 +1,19 @@
+# Michael Jivan
+# 3/29/22
+# Simple script to get you a simple password with phrases
+
+import random 
+
+def generatePassword():
+    password = ""
+    words = ["Cat", "Dog", "RandomWord", "Castle", "Computers"]
+    numbers = "1234567890"
+    special_chars ="!@#$%^&*"
+    
+    password += words[random.randrange(0, len(words))]
+    password += numbers[random.randrange(0, len(numbers))]
+    password += special_chars[random.randrange(0, len(special_chars))]
+    
+    return password
+
+print(generatePassword())
